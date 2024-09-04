3fa8af885d2722d5ee695f0350e234a28e6ecc0b
Felipe F M <felipefm.suporte@gmail.com>
2024-09-04 10:10:09 -0300

    Add files via upload

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-L6B6USSZ.mjs";import"./chunk-6JQLEFCN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-UMLKZGBQ.mjs";import"./chunk-6JQLEFCN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
